f1bbc3c66ce737b96bd303e3a1d98cae4b29d0e1
tan <haitanle88@gmail.com>
2019-06-29 17:48:04 -0400

    add menu button to refresh dynamic weather data

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -15,9 +15,14 @@
  */
 package com.example.android.sunshine.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -65,8 +70,23 @@ public final class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String locationQuery) {
-        // TODO (1) Fix this method to return the URL used to query Open Weather Map's API
-        return null;
+        // COMPLETE (1) Fix this method to return the URL used to query Open Weather Map's API
+        Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM,locationQuery)
+                .appendQueryParameter(FORMAT_PARAM,format)
+                .appendQueryParameter(UNITS_PARAM,units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
+
+        URL urlString = null;
+        try{
+            urlString = new URL(builtUri.toString());
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, urlString.toString());
+        return urlString;
     }
 
     /**
